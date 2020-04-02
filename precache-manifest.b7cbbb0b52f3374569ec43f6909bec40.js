188c29d20fbdb79f0a83a0d7b3520f274dab77ec
ufuk-tech-name <ufuk-tech-name@hotmail.com>
2020-04-02 21:11:28 +0300

    Updates

---
+++ precache-manifest.b7cbbb0b52f3374569ec43f6909bec40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea26100acdccb66f0f837602c5679dc",
+    "url": "/personal_trainer/index.html"
+  },
+  {
+    "revision": "6c3d4b455b2bdf0d3659",
+    "url": "/personal_trainer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3cf11dfa7c9589a478bc",
+    "url": "/personal_trainer/static/js/2.1ea66d7f.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/personal_trainer/static/js/2.1ea66d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c3d4b455b2bdf0d3659",
+    "url": "/personal_trainer/static/js/main.c87acfbf.chunk.js"
+  },
+  {
+    "revision": "6ec34fbe60b55f951570",
+    "url": "/personal_trainer/static/js/runtime-main.713eb410.js"
+  }
+]);
